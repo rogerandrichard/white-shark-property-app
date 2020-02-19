0f53376b9b98296c905978843a16716b16a56d58
Roger Singh <roger@whiteshark.ca>
2020-02-18 22:58:01 -0500

    tenant functionality II

--- src/components/property/PhotoPage.js
+++ src/components/property/PhotoPage.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PhotoPage = (props)=>(
   <div className='photo__content'>
-    <img className='largephoto__image' src='/images/shantz.jpg' />
-    <img className='largephoto__image' src='/images/man.jpg' />
+    <img className='largephoto__image' src='/images/me.jpg' />
+    <img className='largephoto__image' src='/images/shark.jpg' />
   </div>
 )
 
